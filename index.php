19f7a7126c47b7d24c0e235ac626a2c2b002a67f
DEEPANSHU-KUSHWAHA <78898988+DEEPANSHU-KUSHWAHA@users.noreply.github.com>
2026-02-15 23:57:02 +0530

    Navbar Updated

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
-<?php
-// index.php
-include 'app/navbar/index.php';
-?>
-
-<h1>Welcome to Varta</h1>
-<p>This is the homepage content.</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Varta</title>
+    <link rel="stylesheet" href="public/css/navbar.css">
+</head>
+<body>
+    <?php include __DIR__ . '/app/navbar/index.php'; ?>
+</body>
+</html>
