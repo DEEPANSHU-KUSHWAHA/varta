e9db8f57ea590fc3bfbf191d0b5328629660ffe9
DEEPANSHU-KUSHWAHA <78898988+DEEPANSHU-KUSHWAHA@users.noreply.github.com>
2026-02-15 22:59:42 +0530

    test 9

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,15 +1,22 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
+    /**
+     * @covers \App\Calculator::add
+     */
     public function testAdditionWorks()
     {
         $this->assertEquals(4, 2 + 2);
     }
 
+    /**
+     * @covers \App\StringHelper::contains
+     */
     public function testStringContains()
     {
-        $this->assertStringContainsString('Varta', 'Welcome to Varta');
+        $this->assertStringContainsString('foo', 'foobar');
     }
 }
