454acd1488a5e0a675f57967fa865a73446509e0
DEEPANSHU-KUSHWAHA <78898988+DEEPANSHU-KUSHWAHA@users.noreply.github.com>
2026-03-02 13:42:02 +0530

    fixing 3

--- resources/pagination.php
+++ resources/pagination.php
@@ -1,24 +1,26 @@
 <?php
-/**
- * Render pagination controls
- *
- * @param int $currentPage Current page number
- * @param int $totalPages Total number of pages
- * @param string $endpoint The PHP file to fetch (e.g., 'notifications')
- */
-function render_pagination(int $currentPage, int $totalPages, string $endpoint): void {
-    if ($totalPages <= 1) return; // no pagination needed
-    ?>
-    <div class="pagination">
-        <?php if ($currentPage > 1): ?>
-            <a href="#" class="page-link" data-endpoint="<?php echo $endpoint; ?>" data-page="<?php echo $currentPage - 1; ?>">Previous</a>
-        <?php endif; ?>
+if (!function_exists('render_pagination')) {
+    /**
+     * Render pagination controls
+     *
+     * @param int $currentPage Current page number
+     * @param int $totalPages Total number of pages
+     * @param string $endpoint The PHP file to fetch (without .php)
+     */
+    function render_pagination(int $currentPage, int $totalPages, string $endpoint): void {
+        if ($totalPages <= 1) return;
+        ?>
+        <div class="pagination">
+            <?php if ($currentPage > 1): ?>
+                <a href="#" class="page-link" data-endpoint="<?php echo $endpoint; ?>" data-page="<?php echo $currentPage - 1; ?>">Previous</a>
+            <?php endif; ?>
 
-        <span>Page <?php echo $currentPage; ?> of <?php echo $totalPages; ?></span>
+            <span>Page <?php echo $currentPage; ?> of <?php echo $totalPages; ?></span>
 
-        <?php if ($currentPage < $totalPages): ?>
-            <a href="#" class="page-link" data-endpoint="<?php echo $endpoint; ?>" data-page="<?php echo $currentPage + 1; ?>">Next</a>
-        <?php endif; ?>
-    </div>
-    <?php
+            <?php if ($currentPage < $totalPages): ?>
+                <a href="#" class="page-link" data-endpoint="<?php echo $endpoint; ?>" data-page="<?php echo $currentPage + 1; ?>">Next</a>
+            <?php endif; ?>
+        </div>
+        <?php
+    }
 }
